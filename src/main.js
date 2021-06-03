e6f6811fe0f549378bc8ce71e171a588090b6273
pospi <pospi@spadgos.com>
2021-06-03 17:07:44 +1000

    entrypoint for Rollup bundler

---
+++ src/main.js
@@ -0,0 +1,14 @@
+/**
+ * Site scripts bundle manifest
+ *
+ * @package: EK website
+ * @since:   2021-06-03
+ */
+
+import 'd3-selection'
+import 'd3-transition'
+import 'd3-dispatch'
+import 'd3-timer'
+import 'd3-color'
+
+import './ek-logo'
