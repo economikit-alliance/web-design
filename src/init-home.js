fdcb0cd6dfe89c3572fe2fc97b95e801fd7ca8b5
pospi <pospi@spadgos.com>
2021-07-31 19:14:16 +1000

    trigger logo change on subheader scroll on homepage

--- src/init-home.js
+++ src/init-home.js
@@ -5,7 +5,7 @@ function initEKLogo() { randomizeEKLogo(18000, 3, 2000); }
 initEKLogo();
 
 // reload logo and colours when scrolling to new sections
-const targets = document.querySelectorAll('h1');
+const targets = document.querySelectorAll('h1,h2');
 const options = {
   root: null,
   threshold: 0.25, // 0 - 1 this work as a trigger.
